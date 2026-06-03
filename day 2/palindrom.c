82d3d8e8ea2767e6df17a27fb4a445906e069418
25114ew006-ux <25114ew006@glbitm.ac.in>
2026-06-03 16:53:10 +0530

    Update palindrom.c

--- day 2/palindrom.c
+++ day 2/palindrom.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main() {
+    int n, original, reverse = 0, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    original = n;
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+    if (original == reverse)
+        printf("%d is a Palindrome\n", original);
+    else
+        printf("%d is NOT a Palindrome\n", original);
+    return 0;
+}
